b321415d585a40e35deb3ae4c117159425d7a470
brett-todd <39944380+brett-todd@users.noreply.github.com>
2025-10-12 19:54:49 +0100

    Update fastcon_light.h

--- components/fastcon/fastcon_light.h
+++ components/fastcon/fastcon_light.h
@@ -1,3 +1,4 @@
+
 #pragma once
 
 #include "esphome/core/component.h"
@@ -6,25 +7,18 @@
 namespace esphome {
 namespace fastcon {
 
-enum class DefaultFirstOnMode {
-  RGB_WHITE,
-  WARM_WHITE
-};
-
-struct LastLevels {
-  float r{0}, g{0}, b{0}, cw{0}, ww{0};
-};
-
 class FastconController; // fwd decl
 
-class FastconLight : public light::LightOutput {
+class FastconLight : public Component, public light::LightOutput {
  public:
+  FastconLight() = default;
+  explicit FastconLight(int light_id) { this->light_id_ = static_cast<uint8_t>(light_id); }
+
   void set_controller(FastconController *c) { controller_ = c; }
   void set_light_id(uint8_t id) { light_id_ = id; }
   void set_supports_cwww(bool v) { supports_cwww_ = v; }
-  void set_default_first_on(DefaultFirstOnMode m) { default_first_on_ = m; }
 
-  // LightOutput overrides
+  // LightOutput interface
   light::LightTraits get_traits() override;
   void write_state(light::LightState *state) override;
 
@@ -32,10 +26,7 @@ class FastconLight : public light::LightOutput {
   FastconController *controller_{nullptr};
   uint8_t light_id_{0};
   bool supports_cwww_{false};
-  DefaultFirstOnMode default_first_on_{DefaultFirstOnMode::WARM_WHITE};
-  bool has_last_{false};
-  LastLevels last_{};
 };
 
-} // namespace fastcon
-} // namespace esphome
+}  // namespace fastcon
+}  // namespace esphome
