abadc73bb806d51c79f49365b023b156cc584591
brett-todd <39944380+brett-todd@users.noreply.github.com>
2025-10-12 18:18:27 +0100

    Update fastcon_light.h

--- components/fastcon/fastcon_light.h
+++ components/fastcon/fastcon_light.h
@@ -1,35 +1,41 @@
 #pragma once
 
-#include <array>
-#include <vector>
 #include "esphome/core/component.h"
 #include "esphome/components/light/light_output.h"
-#include "fastcon_controller.h"
 
-namespace esphome
-{
-    namespace fastcon
-    {
-        enum class LightState
-        {
-            OFF,
-            WARM_WHITE,
-            RGB
-        };
+namespace esphome {
+namespace fastcon {
 
-        class FastconLight : public Component, public light::LightOutput
-        {
-        public:
-            FastconLight(uint8_t light_id) : light_id_(light_id) {}
+enum class DefaultFirstOnMode {
+  RGB_WHITE,
+  WARM_WHITE
+};
 
-            void setup() override;
-            light::LightTraits get_traits() override;
-            void write_state(light::LightState *state) override;
-            void set_controller(FastconController *controller);
+struct LastLevels {
+  float r{0}, g{0}, b{0}, cw{0}, ww{0};
+};
 
-        protected:
-            FastconController *controller_{nullptr};
-            uint8_t light_id_;
-        };
-    } // namespace fastcon
+class FastconController; // fwd decl
+
+class FastconLight : public light::LightOutput {
+ public:
+  void set_controller(FastconController *c) { controller_ = c; }
+  void set_light_id(uint8_t id) { light_id_ = id; }
+  void set_supports_cwww(bool v) { supports_cwww_ = v; }
+  void set_default_first_on(DefaultFirstOnMode m) { default_first_on_ = m; }
+
+  // LightOutput overrides
+  light::LightTraits get_traits() override;
+  void write_state(light::LightState *state) override;
+
+ protected:
+  FastconController *controller_{nullptr};
+  uint8_t light_id_{0};
+  bool supports_cwww_{false};
+  DefaultFirstOnMode default_first_on_{DefaultFirstOnMode::WARM_WHITE};
+  bool has_last_{false};
+  LastLevels last_{};
+};
+
+} // namespace fastcon
 } // namespace esphome
